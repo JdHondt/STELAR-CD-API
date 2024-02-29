000ccd87562d1b0ba76083008a31929ba3abba89
Jens d'Hondt <j.e.dhondt@student.tue.nl>
2024-02-29 15:13:28 +0100

    First working version of /cd/run

--- src/main/java/io/github/cdapi/CdController.java
+++ src/main/java/io/github/cdapi/CdController.java
@@ -1,8 +1,25 @@
 package io.github.cdapi;
 
+import _aux.lib;
+import algorithms.performance.CorrelationDetective;
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import core.RunParameters;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import io.swagger.annotations.ApiOperation;
+import netscape.javascript.JSObject;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import queries.QueryTypeEnum;
+import queries.ResultSet;
+import similarities.SimEnum;
+
+import java.util.Arrays;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/cd")
@@ -10,4 +27,71 @@ import org.springframework.web.bind.annotation.RestController;
 public class CdController {
 
 
+    @PostMapping("/run")
+    @ApiOperation(value = "Run a Correlation Detective job", response = String.class)
+    public ResponseEntity runCdJob(
+            @RequestParam(value = "inputPath") String inputPath,
+            @RequestParam(value = "simMetricName") String simMetricName,
+            @RequestParam(value = "maxPLeft") String maxPLeftTmp,
+            @RequestParam(value = "maxPRight") String maxPRightTmp,
+            @RequestBody Map<String, String> payLoad
+    ){
+//        Process the request parameters
+        int maxPLeft;
+        int maxPRight;
+        SimEnum simEnum;
+        try {
+            maxPLeft = Integer.parseInt(maxPLeftTmp);
+            maxPRight = Integer.parseInt(maxPRightTmp);
+        } catch (NumberFormatException e){
+            return ResponseEntity.badRequest().body("maxPLeft and maxPRight must be integers");
+        }
+        try{
+            simEnum = SimEnum.valueOf(simMetricName);
+        } catch (IllegalArgumentException e){
+            return ResponseEntity.badRequest().body(String.format("Unsupported simMetricName: %s, please choose any of: %s",
+                    simMetricName, Arrays.toString(SimEnum.values())));
+        }
+
+//        Create a RunParameters object
+        RunParameters runParameters = new RunParameters(inputPath, simEnum, maxPLeft, maxPRight);
+
+//        Set some default parameters
+        runParameters.setQueryType(QueryTypeEnum.TOPK);
+        runParameters.setParallel(false);
+//        runParameters.setLogLevel(Level.WARNING);
+
+//        Parse the rest of the parameters in the payload, and set them in the RunParameters object
+        for (Map.Entry<String, String> entry : payLoad.entrySet()){
+            try{
+//                Try to parse the value
+                Object value = lib.parseString(entry.getValue());
+                runParameters.set(entry.getKey(), value);
+            } catch (NoSuchFieldException | IllegalAccessException e){
+                return ResponseEntity.badRequest().body(String.format("Unsupported parameter: %s", entry.getKey()));
+            }
+        }
+
+//        Remove all current global logger handlers
+        Arrays.stream(Logger.getGlobal().getHandlers()).forEach(Logger.getGlobal()::removeHandler);
+
+
+//        Run the query
+        CorrelationDetective cd = new CorrelationDetective(runParameters);
+        cd.run();
+
+//        Prepare the response
+        Gson gson = new Gson();
+        JsonElement runParametersJsonElement = runParameters.toJsonElement();
+        JsonElement statBagJsonElement = runParameters.getStatBag().toJsonElement();
+        JsonElement resultSetJsonElement = runParameters.getResultSet().toJsonElement(gson);
+
+//        Combine the three json elements into one
+        JsonObject response = new JsonObject();
+        response.add("run_parameters", runParametersJsonElement);
+        response.add("run_statistics", statBagJsonElement);
+        response.add("results", resultSetJsonElement);
+
+        return new ResponseEntity<>(response.toString(), HttpStatus.OK);
+    }
 }
--- src/main/java/io/github/cdapi/StelarCdApiApplication.java
+++ src/main/java/io/github/cdapi/StelarCdApiApplication.java
@@ -1,10 +1,13 @@
 package io.github.cdapi;
 
+import ch.qos.logback.classic.Level;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.Logger;
 
 @SpringBootApplication
 public class StelarCdApiApplication {

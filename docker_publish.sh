556fbb93257c4ba7acbd2ac53b0453ecf1ba7ea8
Jens d'Hondt <45044727+JdHondt@users.noreply.github.com>
2024-03-01 11:00:48 +0100

    Delete docker_publish.sh

--- docker_publish.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Make sure to package through intellij maven plugin
-
-docker login -u correlationdetective -p 'f73qpvPUrxg6^&gs!tROb'
-
-docker build -t correlationdetective/stelar-api:latest .
-
-docker push correlationdetective/stelar_api:latest
